e1f8070419ecbe4fc312317a4f36457f08dd2f63
João Pieri <jpieri@icloud.com>
2025-04-09 17:48:22 -0300

    a

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -1,4 +1,4 @@
-var vel =50.5
+var vel =70.5
 console.log(`A velocidade do seu carro é ${vel} km/h`)
 if(vel > 60){ // CONDIÇÃO SIMPLES
     console.log('Você ultrapassou a velocidade permitida. MULTADO!')
